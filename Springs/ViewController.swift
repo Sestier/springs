3a044aec7ee9f3eea0abd3b4fa4541ec596fd3ea
Maestro <Maestro@imac-de-maestro-5.local>
2018-01-22 18:26:56 -0700

    Animación completa de un botón

--- Springs/ViewController.swift
+++ Springs/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var vwCuadro1: UIView!
+    @IBOutlet weak var vwCuadro2: UIView!
+    
+    var azulAnimado = false;
+    
+    @IBAction func btnCuadroAzul(_ sender: Any) {
+        if(azulAnimado){
+            UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro1.center.y -= 200
+                self.vwCuadro1.center.x -= 50
+            }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro1.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: nil)})
+        }else {
+            UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro1.center.y += 200
+                self.vwCuadro1.center.x += 50
+            }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro1.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: nil)})
+        }
+        
+        azulAnimado = !azulAnimado
+    }
+    
+    @IBAction func btnCuadroVerde(_ sender: Any) {
+        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+            self.vwCuadro2.center.y += 200
+            self.vwCuadro2.center.x -= 50
+        }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+            self.vwCuadro2.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        }, completion: nil)})
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
