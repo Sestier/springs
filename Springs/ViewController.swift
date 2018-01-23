4c275c2a6a1c0f3211284d105d68619759cd8d25
Maestro <Maestro@imac-de-maestro-5.local>
2018-01-22 18:54:30 -0700

    Ambos botones, terminado.

--- Springs/ViewController.swift
+++ Springs/ViewController.swift
@@ -13,36 +13,74 @@ class ViewController: UIViewController {
     @IBOutlet weak var vwCuadro1: UIView!
     @IBOutlet weak var vwCuadro2: UIView!
     
-    var azulAnimado = false;
+    var azulAnimado = false
+    var verdeAnimado = false
     
-    @IBAction func btnCuadroAzul(_ sender: Any) {
+    var ocupado = false
+    
+    func animarCuadroVerde(){
+        if(verdeAnimado){
+            UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro2.center.y -= 200
+                self.vwCuadro2.center.x += 50
+            }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro2.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: {_ in self.ocupado = false})})
+            
+        }else{
+            if(azulAnimado){
+                animarCuadroAzul()
+            }
+            UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro2.center.y += 200
+                self.vwCuadro2.center.x -= 50
+            }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                self.vwCuadro2.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: {_ in self.ocupado = false})})
+            
+        }
+        verdeAnimado = !verdeAnimado
+    }
+    
+    func animarCuadroAzul(){
         if(azulAnimado){
             UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
                 self.vwCuadro1.center.y -= 200
                 self.vwCuadro1.center.x -= 50
             }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
-                self.vwCuadro1.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
-            }, completion: nil)})
+                self.vwCuadro1.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: {_ in self.ocupado = false})})
         }else {
+            if(verdeAnimado){
+                animarCuadroVerde()
+            }
             UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
                 self.vwCuadro1.center.y += 200
                 self.vwCuadro1.center.x += 50
             }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
-                self.vwCuadro1.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            }, completion: nil)})
+                self.vwCuadro1.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: {_ in self.ocupado = false})})
         }
         
         azulAnimado = !azulAnimado
+        
+    }
+    
+    @IBAction func btnCuadroAzul(_ sender: Any) {
+        if(!ocupado){
+            ocupado = true
+            animarCuadroAzul()
+        }
     }
     
     @IBAction func btnCuadroVerde(_ sender: Any) {
-        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
-            self.vwCuadro2.center.y += 200
-            self.vwCuadro2.center.x -= 50
-        }, completion: { _ in UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
-            self.vwCuadro2.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
-        }, completion: nil)})
+        if(!ocupado){
+            ocupado = true
+            animarCuadroVerde()
     }
+    }
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
